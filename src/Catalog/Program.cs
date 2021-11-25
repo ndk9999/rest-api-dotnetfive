efdd22c4e506d11c1a2cd2b02a6da9012b330c97
ndk9999 <nguyenduykhanh9999@live.com>
2021-11-24 23:20:24 -0800

    Deploy to docker container

--- src/Catalog/Program.cs
+++ src/Catalog/Program.cs
@@ -51,7 +51,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
