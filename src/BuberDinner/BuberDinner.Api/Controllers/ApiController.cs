5f172cfe2789bed2f2712386d78c0d9a7b4e4630
Phuc Nguyen <phuc.nguyen@devsoft.vn>
2022-08-22 09:26:42 +0700

    Add validation behavior by combining MediatR and FluentValidation

--- src/BuberDinner/BuberDinner.Api/Controllers/ApiController.cs
+++ src/BuberDinner/BuberDinner.Api/Controllers/ApiController.cs
@@ -1,6 +1,7 @@
 using BuberDinner.Api.Common.Http;
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BuberDinner.Api.Controllers;
 
@@ -9,10 +10,30 @@ public class ApiController : ControllerBase
 {
     protected IActionResult Problem(List<Error> errors)
     {
+        if (errors.Count == 0)
+        {
+            return Problem();
+        }
+
+        if (errors.All(x => x.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        // Custom logic based on dommand error code
+        // if (errors.All(x => x.NumericType == 123))
+        // {
+        //     return HandleErrorResponse(errors);
+        // }
+
         HttpContext.Items[HttpContextItemKeys.Errors] = errors;
 
-        var firstError = errors[0];
-        var statusCode = firstError.Type switch
+        return Problem(errors[0]);
+    }
+
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
         {
             ErrorType.Conflict => StatusCodes.Status409Conflict,
             ErrorType.Validation => StatusCodes.Status400BadRequest,
@@ -20,6 +41,18 @@ public class ApiController : ControllerBase
             _ => StatusCodes.Status500InternalServerError
         };
 
-        return Problem(statusCode: statusCode, title: firstError.Description);
+        return Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDict = new ModelStateDictionary();
+
+        foreach (var item in errors)
+        {
+            modelStateDict.AddModelError(item.Code, item.Description);
+        }
+
+        return ValidationProblem(modelStateDict);
     }
 }
--- src/BuberDinner/BuberDinner.Application/DependencyInjection.cs
+++ src/BuberDinner/BuberDinner.Application/DependencyInjection.cs
@@ -1,6 +1,6 @@
-using BuberDinner.Application.Services.Authentication;
-using BuberDinner.Application.Services.Authentication.Commands;
-using BuberDinner.Application.Services.Authentication.Queries;
+using System.Reflection;
+using BuberDinner.Application.Common.Behaviors;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,8 +12,16 @@ public static class DependencyInjection
     {
         // services.AddScoped<IAuthenticationCommandService, AuthenticationCommandService>();
         // services.AddScoped<IAuthenticationQueryService, AuthenticationQueryService>();
+        // services.AddScoped<IValidator<RegisterCommand>, RegisterCommandValidator>();
+        // services.AddScoped<IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>, ValidationRegisterCommandBehavior>();
 
         services.AddMediatR(typeof(DependencyInjection).Assembly);
+
+        // Add MediatR pipeline behavior
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        // Add FluentValidation
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
     }
